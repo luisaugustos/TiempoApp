a9fa1a6cd0de54113deda1a32a98663764488139
LUIS <luisaugustos@usal.es>
2023-11-03 10:12:23 +0100

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Luis Augusto Silva on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
